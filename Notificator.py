e638d40089bc6f069bf98818a8790270a502ef50
shayg <shaygali10@gmail.com>
2024-02-16 13:15:27 +0200

    add comment to the Notificator.py

--- Notificator.py
+++ Notificator.py
@@ -1,11 +1,42 @@
 class Notificator:
+    """
+    A class to notify users.
+    This class implements the observer pattern.
+    The register method is implemented in the User class, because we notify just the users who are following another user.
+    """
 
-    def notify_user(self, user_to_notify: "User", notifier: "User", message: str, log: bool = False, extra_message: str = ""):
-        if user_to_notify == notifier:
+    def notify_user(self, user_to_notify: "User", notifier: "User", message: str, log: bool = False,
+                    extra_message: str = ""):
+        """
+        Notify a user with a message.
+        You can also log the message and add an extra message.
+        Args:
+            user_to_notify: The user to notify
+            notifier: The user who is notifying
+            message: The message to notify
+            log: True if you want to log the message (Default to False).
+            extra_message: An extra message to add (Default to an empty string).
+
+        Returns:
+            None
+
+        """
+        if user_to_notify == notifier:  # Don't notify the user who is notifying
             return
 
-        user_to_notify.notify(message, log,extra_message)
+        # user the notify method from the User class
+        user_to_notify.notify(message, log, extra_message)
 
     def notify_all_followers(self, caller: "User", message: str, log: bool = False):
-        for user in caller.followers:
+        """
+        Notify all followers of a user.
+        Args:
+            caller: The user who is calling the method
+            message: The message to notify
+            log: True if you want to log the message (Default to False).
+
+        Returns:
+            None
+        """
+        for user in caller.followers:  # Notify all followers
             self.notify_user(user, caller, message, log)
